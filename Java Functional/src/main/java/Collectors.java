7a5be2b40dd92c55a19777a3cbd85b448a1ec8e6
PC <AndreyVladimirov1ch@yandex.ru>
2020-04-19 00:03:50 +0300

    add collection producing

--- Java Functional/src/main/java/Collectors.java
+++ Java Functional/src/main/java/Collectors.java
@@ -6,4 +6,26 @@ public class Collectors {
     downstream collectors.
      */
 
+    public static void main(String[] args) {
+        // Производство коллекций
+        // У любого стрима есть метод collect()
+
+//        Предположим, у нас есть список аккаунтов. У каждой учетной записи есть баланс
+//        (Long), уникальный номер (String) и некоторые другие поля.
+
+//        List<String> numberList = accounts.stream()
+//                .map(Account::getNumber)
+//                .collect(Collectors.toList());
+
+//        Set<String> numberSet = accounts.stream()
+//                .map(Account::getNumber)
+//                .collect(Collectors.toSet());
+
+//        Map<String, Long> numberToBalanceMap = accounts.stream()
+//                .collect(Collectors.toMap(Account::getNumber, Account::getBalance));
+
+//        stream().collect(Collectors.toCollection(HashSet::new));
+
+
+    }
 }
