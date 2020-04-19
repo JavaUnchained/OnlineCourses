3a1208be696806aa98b160615a9aa8b28aba6814
PC <AndreyVladimirov1ch@yandex.ru>
2020-04-19 20:32:25 +0300

    Monads

--- Java Functional/src/main/java/Monads.java
+++ Java Functional/src/main/java/Monads.java
@@ -1,2 +1,12 @@
+import java.util.Optional;
+
 public class Monads {
+    public static void main(String[] args) {
+        Optional<Integer> optionalInteger = Optional.of(25);
+        System.out.println(optionalInteger);
+        System.out.println(optionalInteger.get());
+        System.out.println(optionalInteger.isPresent());
+        System.out.println(optionalInteger.map(x -> x * x).get());
+
+    }
 }
